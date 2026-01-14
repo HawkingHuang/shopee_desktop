600c04e77ab05b1acc0d34bfcf332b3704ca3ce2
HawkingHuang <math0915@gmail.com>
2026-01-15 01:43:14 +0800

    feat: checkout custom styles/always scroll to top

--- src/ui/AppLayout.tsx
+++ src/ui/AppLayout.tsx
@@ -2,11 +2,13 @@ import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import Chat from "../components/Chat/Chat";
+import ScrollToTop from "./ScrollToTop";
 
 function AppLayout() {
   const location = useLocation();
   return (
     <>
+      <ScrollToTop />
       {location.pathname === "/login" || location.pathname === "/signup" ? null : <Header />}
       <Outlet />
       <Footer />
